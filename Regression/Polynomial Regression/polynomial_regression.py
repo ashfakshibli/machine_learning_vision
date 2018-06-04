2634cb9e7df1ffbaf429532b727811b999ed6cbd
Ashfak Md. Shibli <shibli.emon@gmail.com>
2018-06-04 12:56:37 +0600

    Polynomial Regression. Visualizing and degree up the polinomial regression and small step size for good curve.

--- Regression/Polynomial Regression/polynomial_regression.py
+++ Regression/Polynomial Regression/polynomial_regression.py
@@ -45,12 +45,27 @@ lin_reg.fit(X, y)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+poly_reg = PolynomialFeatures(degree = 4) # 2, 3, 4
 X_poly = poly_reg.fit_transform(X)
 lin_reg2 = LinearRegression()
 lin_reg2.fit(X_poly, y)
 
+# Visualizing the linear Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
 
 
-
+# Visualizing the Polynomial Regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid)), 1)
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
 
